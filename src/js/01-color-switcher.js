11a4f4b9ddb68ca5f393ea4ac80b5485952d9786
RuslanKabachok <ruslan.kabachok@gmail.com>
2021-12-26 22:03:56 +0200

    task #1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+  startBtnEl: document.querySelector('button[data-start]'),
+  stopBtnEl: document.querySelector('button[data-stop]'),
+  bodyEl: document.querySelector('body'),
+};
 
+let intervalId;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartClick() {
+  intervalId = setInterval(() => {
+    refs.bodyEl.style.background = getRandomHexColor();
+  }, 1000);
+
+  refs.startBtnEl.setAttribute('disabled', 'disabled');
+}
+
+function onStopClick() {
+  clearInterval(intervalId);
+  refs.startBtnEl.removeAttribute('disabled');
+}
+
+refs.startBtnEl.addEventListener('click', onStartClick);
+refs.stopBtnEl.addEventListener('click', onStopClick);
